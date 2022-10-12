1b9ae02c044e61a095690a1082330938d5f5664a
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-10-13 02:29:58 +0900

    make default config.yml file on setup

--- buildSrc/src/main/kotlin/SetupTask.kt
+++ buildSrc/src/main/kotlin/SetupTask.kt
@@ -36,6 +36,8 @@ open class SetupTask : DefaultTask() {
         )
         projectDir.resolve("src/main/resources").apply(File::mkdirs)
         val buildScript = projectDir.resolve("build.gradle.kts")
+        val rsrcDir = projectDir.resolve("src/main/resources").apply(File::mkdirs)
+        rsrcDir.resolve("config.yml").createNewFile()
         buildScript.writeText(buildScript.readText().replace("@group@", groupId))
         projectDir.resolve("README.md").writeText(
             """
